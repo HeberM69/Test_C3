9a0ab366610f540c6457c0362964e98dd5718b36
Jorge Esteban Madrigal Ramirez <114161329+JEMadrigal@users.noreply.github.com>
2025-06-02 11:18:40 -0600

    Create BinarySearch.java

---
+++ BinarySearch.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int binarySearch(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right + 1) / 2; // Bug similar
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return -1;
+    }
+}
