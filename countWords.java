576d1680f9e435db03b98b3676201f2d0bd80cf5
ceXuar <104097162+cesarcc0110@users.noreply.github.com>
2025-06-03 04:22:07 -0600

    Delete countWords.java

--- countWords.java
+++
@@ -1,9 +0,0 @@
-public int countWords(String s) {
-    int count = 0;
-    for (int i = 0; i < s.length(); i++) {
-        if (s.charAt(i) == ' ' && s.charAt(i - 1) != ' ') { // Bug: i-1 puede ser negativo
-            count++;
-        }
-    }
-    return count + 1;
-}
