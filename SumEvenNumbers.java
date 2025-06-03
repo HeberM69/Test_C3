7cd311a484609cb71efd3c70286404a4d53a150b
ceXuar <104097162+cesarcc0110@users.noreply.github.com>
2025-06-03 04:41:49 -0600

    Delete SumEvenNumbers.java

--- SumEvenNumbers.java
+++
@@ -1,9 +0,0 @@
-public int sumEven(int[] nums) {
-    int sum = 0;
-    for (int n : nums) {
-        if (n % 2 == 1) { // Bug: debería ser == 0
-            sum += n;
-        }
-    }
-    return sum;
-}
