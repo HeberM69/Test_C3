822547592fa002f9942f1b97c5cd8186dacad813
ceXuar <104097162+cesarcc0110@users.noreply.github.com>
2025-06-03 04:43:56 -0600

    Delete MissingNumber.cpp

--- MissingNumber.cpp
+++
@@ -1,7 +0,0 @@
-int missingNumber(vector<int>& nums) {
-    int result = nums.size();
-    for (int i = 0; i < nums.size(); i++) {
-        result ^= i ^ nums[i];
-    }
-    return result ^ 0; // Bug: no es necesario el XOR con 0
-}
