0e0468ac8df340438ffb79af02fe598387aec9f5
ceXuar <104097162+cesarcc0110@users.noreply.github.com>
2025-06-03 04:43:04 -0600

    Delete IsPrime.java

--- IsPrime.java
+++
@@ -1,7 +0,0 @@
-public boolean isPrime(int n) {
-    if (n <= 1) return false;
-    for (int i = 2; i < n / 2; i++) { // Bug: debería ser i <= sqrt(n)
-        if (n % i == 0) return false;
-    }
-    return true;
-}
