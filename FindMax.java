3adb52ded8648be4fbd1b6a15e6e5eb0ec0a077a
ceXuar <104097162+cesarcc0110@users.noreply.github.com>
2025-06-03 04:01:50 -0600

    Delete FindMax.java

--- FindMax.java
+++
@@ -1,9 +0,0 @@
-public int findMax(int[] nums) {
-    int maxVal = nums[0];
-    for (int i = 1; i < nums.length; i++) {
-        if (nums[i] < maxVal) { // Bug: debería ser >
-            maxVal = nums[i];
-        }
-    }
-    return maxVal;
-}
